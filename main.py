5c944b43cad36f03bdee8f0d3a85b842e7649795
Nathaniel Guy <nattybumppo@gmail.com>
2011-08-01 18:09:43 -0700

    Added main.py.

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is main.py, the main file for the custom-homepage-creator.
+#
+# Note that this project will attempt to follow the Google Python Style Guide,
+# available here:
+#
+# http://google-styleguide.googlecode.com/svn/trunk/pyguide.html.
+
+
+
+def main():
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
